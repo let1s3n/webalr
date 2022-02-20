1518c3ac78207d8da523fb05a3205fe8831fcb69
let1s3n <letisendev@gmail.com>
2022-02-19 21:07:23 -0500

    fix

--- pages/nosotros.jsx
+++ pages/nosotros.jsx
@@ -59,7 +59,7 @@ const nosotros = () => {
 
         <section id="vision-section" className="vision-section mb-4">
           <Container fluid="xxl" className="bg-blue2 bg-opacity-10 g-0">
-            <Row className="g-0">
+            <Row className="g-0" xs={1} md={2}>
               <Col>
                 <div className="bg-blue3 d-flex align-items-center justify-content-center">
                   <h2 className="fw-bolder m-0 text-blue2">Visión</h2>
@@ -68,7 +68,7 @@ const nosotros = () => {
                   Lograr ser reconocidos como empresa lider en el Perú en minería y metalurgia de metales preciosos obteniendo la valoración de nuestros colaboradores, grupos de interés y accionistas.
                 </p>
               </Col>
-              <Col xs="auto">
+              <Col>
                 <Image
                   src="/images/vision-col.jpg"
                   alt="Imagen visión"
@@ -80,7 +80,7 @@ const nosotros = () => {
         </section>
         <section id="mision-section" className="mision-section mb-4">
           <Container fluid="xxl" className="bg-yellow2 bg-opacity-10 g-0">
-            <Row className="g-0">
+            <Row className="g-0" xs={1} md={2}>
               <Col>
                 <div className="bg-yellow3 d-flex align-items-center justify-content-center">
                   <h2 className="fw-bolder m-0 text-yellow2">Misión</h2>
@@ -89,7 +89,7 @@ const nosotros = () => {
                   Realizar minería responsable preservando y respetando nuestro entorno y a nuestras comunidades, haciendo la diferencia en el aprovechamiento racional de nuestros activos comprometidos con la responsabilidad social y ambiental.
                 </p>
               </Col>
-              <Col xs="auto">
+              <Col>
 
                 <Image
                   src="/images/mision-col.jpg"
@@ -102,7 +102,7 @@ const nosotros = () => {
         </section>
         <section id="valores-section" className="valores-section mb-4">
           <Container fluid="xxl" className="bg-white g-0">
-            <Row className="g-0">
+            <Row className="g-0" xs={1} md={2}>
               <Col>
                 <div className="bg-white d-flex align-items-center justify-content-center">
                   <h2 className="fw-bolder m-0">Valores</h2>
@@ -112,7 +112,7 @@ const nosotros = () => {
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum numquam optio ipsam ab, id, delectus autem reprehenderit voluptatem iusto quos esse. Impedit hic earum quos repellat beatae explicabo, magni molestias.
                 </p>
               </Col>
-              <Col xs="auto">
+              <Col>
                 <Image
                   src="/images/valores.jpg"
                   alt="Imagen visión"
