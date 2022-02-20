dbc45a77975603552fdf11f6d5a5bf678a9a4eb4
let1s3n <letisendev@gmail.com>
2022-02-19 23:00:07 -0500

    fix

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,29 +1,33 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
-import Image from 'next/image'
-import { Container, Row, Col} from 'react-bootstrap';
-import { ImLinkedin } from "react-icons/im";
+/* import Image from 'next/image' */
+import { Container, Row, Col, Image } from 'react-bootstrap';
+/* import { ImFacebook2,ImYoutube,ImLinkedin,ImTwitter } from "react-icons/im"; */
+/* import { GrFacebook,GrYoutube,GrLinkedin,GrTwitter } from "react-icons/gr"; */
+/* import { FaFacebookSquare,FaYoutubeSquare,FaLinkedin,FaTwitterSquare } from "react-icons/fa"; */
+import { IoLogoFacebook, IoLogoYoutube, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
+
 const Footer = () => {
 
   return (
-    <Container fluid className="main-footer text-white py-3 px-2 py-lg-5 px-lg-5">
-      <Row className="g-0">
-        <Col xs={12} lg={2} xxl={2} className="d-flex justify-content-center align-items-center">
-          
-            <Image className="logo" src="/images/logo-alr-square.png" alt="ALR logo" width={200} height={200}/>
+    <Container fluid className="main-footer bg-blue1 text-white py-3 px-0 py-lg-5 position-relative overflow-hidden">
+      <Row className="g-0 px-2 px-lg-5">
+        <Col xs={12} lg={2} xxl={2} className="d-flex justify-content-center align-items-center" style={{ zIndex: 2 }}>
 
-          
+          <Image className="logo" src="/images/logo-alr-square.png" alt="ALR logo" style={{ width: 200 + 'px', height: 200 + 'px' }} />
+
+
         </Col>
-        <Col xs={12} lg={4} xxl={6} className="d-flex align-items-center justify-content-center mb-4 mb-lg-0">
-          <Row className="w-100 justify-content-md-evenly justify-content-lg-start justify-content-xxl-around" style={{rowGap:1+'rem'}}>
+        <Col xs={12} lg={4} xxl={6} className="d-flex pt-4 justify-content-center mb-4 mb-lg-0" style={{ zIndex: 2 }}>
+          <Row className="w-100 justify-content-md-evenly justify-content-lg-start ps-xl-5 ps-xxl-6" style={{ rowGap: 1 + 'rem' }}>
             <Col xs="auto" className="ms-lg-3">
-              <p className="m-0">
+              <p className="m-0 fs-xxl-5">
                 Planta Metalúrgica Los Rosales <br />
                 VILQUE, PUNO, PERÚ
               </p>
             </Col>
             <Col xs="auto" className="ms-lg-3">
-              <p className="m-0">
+              <p className="m-0 fs-xxl-5">
                 Av. Circunvalación Del Golf Los Inkas 134 <br />
                 Oficina 707 Torre 1 - Santiago De Surco <br />
                 LIMA - PERÚ
@@ -31,17 +35,22 @@ const Footer = () => {
             </Col>
           </Row>
         </Col>
-        <Col xs={6} lg={3} xxl={2} className="d-flex justify-content-center align-items-center">
+        <Col xs={6} lg={3} xxl={2} className="d-flex justify-content-center pt-4" style={{ zIndex: 2 }}> 
           <Row xs={1} className="g-0">
             <Col>
               <p className="m-0 fs-4 fw-600 text-center">Síguenos</p>
             </Col>
             <Col className="text-center">
-              <a href="https://www.linkedin.com/company/mineralosrosales" target="_blank"><ImLinkedin /></a>
+              <div className="d-flex justify-content-center" style={{columnGap:.5+'rem'}}>
+                <a href="#" target="_blank"><IoLogoFacebook className="facebook-icon fs-md-5 fs-xxl-4" /></a>
+                <a href="#" target="_blank"><IoLogoYoutube className="youtube-icon fs-md-5 fs-xxl-4" /></a>
+                <a href="https://www.linkedin.com/company/mineralosrosales" target="_blank"><IoLogoLinkedin className="linkedin-icon fs-md-5 fs-xxl-4" /></a>
+                <a href="#" target="_blank"><IoLogoTwitter className="twitter-icon fs-md-5 fs-xxl-4" /></a>
+              </div>
             </Col>
           </Row>
         </Col>
-        <Col xs={6} lg={3} xxl={2} className="d-flex justify-content-center align-items-center">
+        <Col xs={6} lg={3} xxl={2} className="d-flex justify-content-center pt-4" style={{ zIndex: 2 }}>
           <Row xs={1} className="g-0">
             <Col className="text-center">
               <p className="m-0 fs-5">Políticas</p>
@@ -55,7 +64,7 @@ const Footer = () => {
           </Row>
         </Col>
       </Row>
-
+      <Image className="footer-illustration position-absolute" src="/images/footer-illustration.svg" style={{ zIndex: 1 }} />
     </Container>
   )
 }
