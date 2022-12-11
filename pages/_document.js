b41b41c39e50912d453aad99b7eeb18a9046ff99
let1s3n <letisendev@gmail.com>
2022-12-11 15:12:16 -0500

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,6 @@ class MyDocument extends Document {
     return (
       // Agregamos el idioma que deseemos, aquí por ejemplo español.
       <Html lang="es">
-        {/* <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-          <meta name="title" content="SMRL Acumulación Los Rosales" />
-          <meta name="description" content="Peruanos en busca de la excelencia haciendo minería responsable." />
-          <meta name="keywords" content="empresa minera, minera, minería, acumulación los rosales, minería responsable, minería peruana" />
-        </Head> */}
         <Head />
         <body>
           <Main />
