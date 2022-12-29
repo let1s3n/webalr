9aa976e6546b6de4e661f652b56fc7b4ae7eb171
let1s3n <letisendev@gmail.com>
2022-12-29 14:13:41 -0500

    fix

--- components/templates/Contacto/Contacto.jsx
+++ components/templates/Contacto/Contacto.jsx
@@ -1,13 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { Container } from 'react-bootstrap'
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
+
 import CustomForm from '../../modules/customForm'
+
+
+import Loader from '../../elements/Loader/Loader'
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
+
 const Contacto = () => {
+
+  const { height, width } = useWindowDimensions();
+  const { t } = useTranslation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let transControl = t('transControl', { ns: 'general', returnObjects: true })
+
+    console.log("transControl: ", transControl)
+
+    if (typeof transControl !== 'string' && !(transControl instanceof String)) {
+      setLoading(false);
+    }
+
+  }, [t])
+
   return (
-    <section className="contacto-page py-5 py-custom1-10">
-      <Container fluid="md">
-        <CustomForm />
-      </Container>
-    </section>
+    <>
+      <Loader loading={loading} />
+      <section className="contacto-page py-5 py-custom1-10">
+        <Container fluid="md">
+          <CustomForm />
+        </Container>
+      </section>
+    </>
   )
 }
 
--- components/templates/Prensa/Prensa.jsx
+++ components/templates/Prensa/Prensa.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import { useTranslation } from "react-i18next";
+import Loader from '../../elements/Loader/Loader'
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
 const Prensa = () => {
+
+  const { height, width } = useWindowDimensions();
+  const { t } = useTranslation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let transControl = t('transControl', { ns: 'general', returnObjects: true })
+
+    console.log("transControl: ", transControl)
+
+    if (typeof transControl !== 'string' && !(transControl instanceof String)) {
+      setLoading(false);
+    }
+
+  }, [t])
+
   return (
-    <div>Prensa</div>
+    <>
+      <Loader loading={loading} />
+      <div>Prensa</div>
+    </>
   )
 }
 
