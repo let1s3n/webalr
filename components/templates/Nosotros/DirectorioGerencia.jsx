bffc2935f5dcded818fd98146b8a128070f6824f
let1s3n <letisendev@gmail.com>
2023-04-23 18:24:09 -0500

    fix

--- components/templates/Nosotros/DirectorioGerencia.jsx
+++ components/templates/Nosotros/DirectorioGerencia.jsx
@@ -302,6 +302,34 @@ const DirectorioGerencia = () => {
 
               </Row>
 
+              <Row className='mb-3'>
+                <Col xs={12} lg={6}>
+                  <div className='p-5 directors-management__card bg-primary text-white mb-4 mb-xl-0'>
+                    <div>
+                      <h3>{t('telmo_sagastegui_title1', { ns: 'general' })}</h3>
+                      <div className='border border-secondary w-25 mb-3'></div>
+                      <h6>{t('telmo_sagastegui_title2', { ns: 'general' })}</h6>
+                    </div>
+                    <div className="pt-3">
+                      <p className='mb-0'>{t('telmo_sagastegui_text', { ns: 'general' })}</p>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={12} lg={6}>
+                  <div className='p-5 directors-management__card bg-primary text-white mb-4 mb-xl-0'>
+                    <div>
+                      <h3>{t('jefe_comercial_title1', { ns: 'general' })}</h3>
+                      <div className='border border-secondary w-25 mb-3'></div>
+                      <h6>{t('jefe_comercial_title2', { ns: 'general' })}</h6>
+                    </div>
+                    <div className="pt-3">
+                      <p className='mb-0'>{t('jefe_comercial_text', { ns: 'general' })}</p>
+                    </div>
+                  </div>
+                </Col>
+
+              </Row>
+
             </div>
 
           </Container>
